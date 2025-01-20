1d3efcc1fae7863bc4a5e43cb2c186d928c4fef3
rwtrecs <adolfo@rewrite.com.br>
2025-01-20 08:44:35 -0400

    class: 351

---
+++ php/loops_for.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP</title>
+</head>
+
+<body>
+  <?
+    // while
+    /* while (condicao) {
+      ...
+    } */
+
+    // do while
+    /* do {
+      ...
+    } while (condicao); */
+
+    for ($x = 11; $x <= 10; $x++):
+      echo $x . '<br>';
+    endfor;
+  ?>
+</body>
+</html>
