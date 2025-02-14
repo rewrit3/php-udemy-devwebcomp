3b51c5b20f68180df3f76f418b6244875311189d
rewrit3 <adolfo@rewrite.com.br>
2025-02-14 16:54:45 -0400

    exercise: 007

--- php/exercicios/logica/isquicha/README.md
+++ php/exercicios/logica/isquicha/README.md
@@ -1,6 +1,5 @@
 # Estruturas sequenciais
-- Exercicio 005
+- Exercicio 005 ✅
     - Faça um Programa que converta metros para centímetros. 
-    :white_check_mark:
-- Exercicio 006
+- Exercicio 006 ✅
     - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
---
+++ php/exercicios/logica/isquicha/exercicio_007.php
@@ -0,0 +1,9 @@
+<?php
+
+// Estruturas sequenciais
+// Exercicio 007
+// Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+$side = 5;
+$area = $side * $side;
+
+echo 'O dobro da área de um quadrado com lado ' . $side . ' é: ' . (2 * $area);
