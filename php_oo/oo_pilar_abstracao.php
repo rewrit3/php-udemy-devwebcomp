3014d3993d3a9bfd04e8862a7b8b937cd3ebd432
rewrit3 <adolfo@rewrite.com.br>
2025-02-14 15:54:20 -0400

    class: 374

--- php_oo/oo_pilar_abstracao.php
+++ php_oo/oo_pilar_abstracao.php
@@ -3,13 +3,38 @@
 // modelo
 class Funcionario {
     // atributos
-    public $nome = 'Adolfo';
-    public $telefone = '65 9 9688 8989';
-    public $numeroFilhos = 2;
+    public $nome = null;
+    public $telefone = null;
+    public $numeroFilhos = null;
+
+    // getters e setters
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setTelefone($telefone) {
+        $this->telefone = $telefone;
+    }
+
+    function setNumeroFilhos($numeroFilhos) {
+        $this->numeroFilhos = $numeroFilhos;
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getNumeroFilhos() {
+        return $this->numeroFilhos;
+    }
+
+    function getTelefone() {
+        return $this->telefone;
+    }
 
     // métodos
     function resumirCadastroFuncionario() {
-        return "$this->nome possui $this->numeroFilhos filho(s). <br>";
+        return "$this->nome possui $this->numeroFilhos filho(s). <br> Seu telefone é $this->telefone. <br>";
     }
 
     function modificarNumeroFilhos($numeroFilhos) {
@@ -17,16 +42,30 @@ class Funcionario {
     }
 }
 
-$funcionario1 = new Funcionario();
-echo $funcionario1->resumirCadastroFuncionario();
+// $funcionario1 = new Funcionario();
+// echo $funcionario1->resumirCadastroFuncionario();
 
-$funcionario1->modificarNumeroFilhos(3);
+// $funcionario1->modificarNumeroFilhos(3);
+// echo $funcionario1->resumirCadastroFuncionario();
+
+// echo '<hr>';
+
+// $funcionario2 = new Funcionario();
+// echo $funcionario2->resumirCadastroFuncionario();
+
+// $funcionario2->modificarNumeroFilhos(1);
+// echo $funcionario2->resumirCadastroFuncionario();
+
+$funcionario1 = new Funcionario();
+$funcionario1->setNome('Adolfo');
+$funcionario1->setTelefone('(65) 9 9688-8989');
+$funcionario1->setNumeroFilhos(2);
 echo $funcionario1->resumirCadastroFuncionario();
 
 echo '<hr>';
 
 $funcionario2 = new Funcionario();
-echo $funcionario2->resumirCadastroFuncionario();
-
-$funcionario2->modificarNumeroFilhos(1);
-echo $funcionario2->resumirCadastroFuncionario();
+$funcionario2->setNome('Sheila');
+$funcionario2->setTelefone('(66) 9 9999-9999');
+$funcionario2->setNumeroFilhos(0);
+echo $funcionario2->getNome() . ' possui ' . $funcionario2->getNumeroFilhos() . ' filho(s). <br> Seu telefone é ' . $funcionario2->getTelefone();
