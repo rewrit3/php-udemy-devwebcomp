02fabf89055680212a3caca037a24a46a18f47f3
rewrit3 <adolfo@rewrite.com.br>
2025-03-14 09:21:48 -0400

    class: 518

--- jquery/app_dashboard/script.js
+++ jquery/app_dashboard/script.js
@@ -1,3 +1,29 @@
 $(document).ready(() => {
+  // $('#link-dashboard').on('click', () => {
+  //   $('#pagina').load('index.html');
+  // });
 
+  $('#link-documentacao').on('click', () => {
+    // $('#pagina').load('documentacao.html');
+
+    $.get('documentacao.html', data => {
+      $('#pagina').html(data);
+    });
+
+    // $.post('documentacao.html', data => {
+    //   $('#pagina').html(data);
+    // });
+  });
+
+  $('#link-suporte').on('click', () => {
+    // $('#pagina').load('suporte.html');
+
+    $.get('suporte.html', data => {
+      $('#pagina').html(data);
+    });
+
+    // $.post('suporte.html', data => {
+    //   $('#pagina').html(data);
+    // });
+  });
 })
