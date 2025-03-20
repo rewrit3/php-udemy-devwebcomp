deb96dfe709ae224cc5f306938cbb46f39694faa
rewrit3 <adolfo@rewrite.com.br>
2025-03-20 09:12:33 -0400

    class: 524

--- php_mvc/miniframework/public/index.php
+++ php_mvc/miniframework/public/index.php
@@ -1,1 +1,1 @@
-<?php echo 'OK';
+<?php require_once('../vendor/autoload.php');
---
+++ php_mvc/miniframework/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rewrite/miniframework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4f0c71ed801e73f675bc3ab3e584180e6a887537',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rewrite/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4f0c71ed801e73f675bc3ab3e584180e6a887537',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
