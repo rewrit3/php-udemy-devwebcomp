003b8378a87435ceb5dc969ee04c54d4cd36b4f9
rewrit3 <adolfo@rewrite.com.br>
2025-05-08 08:57:58 -0400

    exercise: 046

---
+++ php/exercicios/logica/isquicha/exercicio_046.php
@@ -0,0 +1,19 @@
+<?php
+// Estruturas de repetição
+// Exercicio 046
+/* 
+  Faça um programa que peça uma nota, entre zero e dez. 
+  Mostre uma mensagem caso o valor seja inválido e continue pedindo 
+  até que o usuário informe um valor válido.
+*/
+// Solicita a nota ao usuário
+echo 'Digite uma nota entre 0 e 10: ';
+$grade = trim(fgets(STDIN));
+
+while (!is_numeric($grade) or $grade < 0 or $grade > 10) {
+  echo 'Valor inválido. Por favor, digite uma nota válida (0 a 10): ';
+
+  $grade = trim(fgets(STDIN));
+}
+
+echo "Nota válida: $grade \n";
